703e6bdb49346849ee4f1165bec3279077a2fa62
Sergey Demidenko <dcerge@gmail.com>
2013-05-07 02:13:23 -0600

    Some updates in config/main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -27,7 +27,7 @@ return array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>false,
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('192.168.0.*', '127.0.0.1','::1'),
+			'ipFilters'=>array('192.168.0.*', '127.0.0.1','::1'), 
 		),
 	),
 
