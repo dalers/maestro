28a87f74bd7ebb03acc1b46f4c375f490d482e14
Dale Scott <dale@dalescott.net>
2013-11-17 10:49:06 -0700

    updated

--- protected/data/setup.sh
+++ protected/data/setup.sh
@@ -11,55 +11,50 @@
 #
 
 echo
-echo "Performing Maestro SETUP on server..."
+echo "Set up maestro..."
 echo ""
 
-# Set shell scripts executable
-echo "Setting shell scripts executable..."
+echo "Set shell scripts executable..."
 chmod u+x /usr/local/www/maestro/protected/data/*.sh
 chmod u+x /usr/local/www/maestro/protected/yiic
 echo ""
 
-# Ensure Maestro Yii runtime directory is read-write
-echo "Setting Maestro Yii runtime directory to read-write..."
+echo "Set permissions on directories written to
+chmod a+rw /usr/local/www/maestro/assets/
 chmod a+rw /usr/local/www/maestro/runtime/
 echo ""
 
-# Create log file for export-legacy.sh & load-maestro.sh scripts
-# DO NOT USE - LOGGING NOT IMPLEMENTED
-#echo "Creating log file..."
+#echo "Create log file for export_current and load_current shell scripts..."
+# LOGGING NOT IMPLEMENTED
 #touch /usr/home/samba/maestro/legacy-export-import.log
-# do not give Windows clients write permission
+# give system cron user write permission to log file, but not Windows clients accessing share
 #chown nobody:wheel /usr/home/samba/maestro/maestro-import-from-legacy.log
 #chmod a+rw /usr/home/samba/maestro/maestro-import-from-legacy.log
 #echo ""
 
-# create csv dir on windows share
-echo "Removing csv dir on Windows share if exists..."
-rm -r /usr/home/samba/maestro/csv
-echo "Recreating csv dir..."
+echo "Create csv dir in maestro file directory for export_current shell script"
 mkdir /usr/home/samba/maestro/csv
 chmod a+xrw /usr/home/samba/maestro/csv
 echo ""
 
-echo "Copying Parts&Vendors(TM) database to Windows share..."
+echo "Copy demo Parts&Vendors(TM) database to Windows share..."
 cp /usr/local/www/maestro/protected/data/mdb/*.mdb /usr/home/samba/maestro/
 chmod a+rw /usr/home/samba/maestro/*.mdb
 echo ""
 
-echo "Copying default csv files to Windows share..."
+echo "Bootstrap export_current process with initial csv files..."
 cp /usr/local/www/maestro/protected/data/csv/*.csv /usr/home/samba/maestro/csv/
 chmod a+rw /usr/home/samba/maestro/csv/*.csv
 echo ""
 
-echo "Copying spreadsheet files to Windows share..."
+echo "Copy demo spreadsheet files to Windows share..."
 cp /usr/local/www/maestro/protected/data/excel/*.xlsx /usr/home/samba/maestro/
-# make read/write for Windows clients to update (NOT SECURE!)
+# make read/write for Windows clients to update
 chown nobody:wheel /usr/home/samba/maestro/*.xlsx
 chmod a+rw /usr/home/samba/maestro/*.xlsx
 echo ""
 
-echo "Copying README to Windows share..."
+echo "Copy README to Windows share..."
 cp /usr/local/www/maestro/protected/data/README-winshare.txt /usr/home/samba/maestro/README.txt
 # ensure file has Windows EOL, but do not give write permission to Windows clients
 flip -u /usr/home/samba/maestro/README.txt
@@ -67,8 +62,8 @@ flip -u /usr/home/samba/maestro/README.txt
 #chmod a+rw /usr/home/samba/maestro/README.txt
 echo ""
 
-#echo "Loading Maestro database from CSV directory..."
-#/usr/local/www/maestro/protected/data/load_maestro_unix.sh
+#echo "Load current data from CSV files..."
+#/usr/local/www/maestro/protected/data/load_current_unix.sh
 #echo ""
 
 # cleanup
