873067574a6e25641f1e12a08cea084c5ebdd60a
dcerge <dcerge@gmail.com>
2013-11-29 08:15:21 -0700

    Issues #17 and #16 fixed

--- protected/config/main.php
+++ protected/config/main.php
@@ -122,7 +122,7 @@ return array(
         'pagesize' => 30,
         'partListPageSize' => 20,
         'PDFconverter' => 3, // 1 - wkhtmltopdf, 2 - mpdf, 3 - html2pdf
-        'PDFPageSize' => 'A4', // allowed 'A4', 'Letter'
+        'PDFPageSize' => 'Letter', // allowed 'A4', 'Letter'
         'Path2Wkhtmltopdf' => "protected/bin/wkhtmltopdf-i386", // path and name of Path2Wkhtmltopdf binary that is used for PDF convertation
         //'Path2Wkhtmltopdf' => "/opt/wkhtmltopdf-i386",
 	),
