7774fbe04fe07d3a1619af2c70e8bab073b9e3e4
Dale Scott <dale@dalescott.net>
2013-12-18 22:49:18 -0700

    plm run_iterations test script works

--- protected/data/run_iterations.sh
+++ protected/data/run_iterations.sh
@@ -1,37 +1,48 @@
 #!/bin/sh
 #
-# Run through iterations in fast motion
-# - execute from parent of extracted SCC demo data tar file
+# Step through iteration 1 to 4
+# - TODO add maestro "load current" after each iteration
 #
-# - assumes maestro fileshare directory structure exists
+# To use first extract SCC demo data tar file to current directory
+# $ cd /home/dirname
+# $ tar -xzf maestro-scc-files.tar.gz
+# $ cd maestro
+# $ cp /usr/local/www/maestro/protected/data/run_iterations.sh .
+# $ chmod a+x ./run_iterations.sh
+#
+# Assumes fileshare directory structure:
 #   /usr/home/samba/maestro					- root of fileshare
 #   /usr/home/samba/maestro/remotefs/       - simulated remote fs (optional)
 #   /usr/home/samba/maestro/vault           - document vault
 #   /usr/home/samba/maestro/vault.backup    - document backups (optional)
 #
-#  - assumes SCC demo data tar file extracted to current directory
-#    > tar -xzf maestro-scc-files.tar.gz
-#
 #    maestro
-#    |-- pv-1.mdb                      P&V mdb after iteration
+#    |-- pv-1.mdb                      P&V(TM) mdb
 #    |-- pv-2.mdb
 #    |-- pv-3.mdb
 #    |-- pv-4.mdb
-#    |-- pv-1.csv.tar.gz               csv mdb-export from P&V mdb after iteration
-#    |-- pv-2.csv.tar.gz
-#    |-- pv-3.csv.tar.gz
-#    |-- pv-4.csv.tar.gz
-#    |-- vault-1                       vault state after iteration
+#    |-- csv-1                         mdb-export from P&V(TM) mdb
+#    |   |-- issue.csv
+#    |   |-- person.cvs
+#    |   |-- ...
+#    |   `-- stock_serial.csv
+#    |-- csv-2
+#    |   `-- ...
+#    |-- csv-3
+#    |   `-- ...
+#    |-- csv-4
+#    |   `-- ...
+#    |-- vault-1                       file vault
 #    |   |-- 20000001
 #    |   |   |-- 20000001_WI-00.odt
 #    |   |   |-- 20000001_WI-00.pdf
 #    |   |   `-- 20000001_WI-00_graphics.odg
 #    |   |-- 20000002
 #    |   |-- 20000003
-#    |   |...
+#    |   |-- ...
 #    |   `-- 90000013
 #    |-- vault-2
-#    |   `...
+#    |   `-- ...
 #    |-- vault-3
 #    |   `...
 #    `-- vault-4
@@ -41,15 +52,28 @@
 # startup
 # -----------------
 
-# remove any existing files from simulated remote fs
-rm -r /home/samba/maestro/remotefs/*
+# remove any output files in current directory
+rm ./run_iterations_rsync*.log
 
-# remove any existing files from maestro file share
+# remove any existing files
+rm /home/samba/maestro/*
+# simulated remote fs
+rm -r /home/samba/maestro/remotefs/*
+# file vault
 rm -r /home/samba/maestro/vault/*
-rm -r /home/samba/maestro/vault.backup/*
+#rm -r /home/samba/maestro/vault.backup/*
+# current and previous csv
+rm /home/samba/maestro/csv/*
+rm /home/samba/maestro/csv.old/*
 
 # ensure required fileshare owner/mod (web server must have read/write permission)
 chown -R www:www /home/samba/maestro/
+
+# set local files as if created by owner www:www
+chown -R www:www ./*
+
+# copy spreadsheets to maestro share
+cp ./excel/*.xlsx /home/samba/maestro/
 
 # set access/modification dates for each iteration vault
 find ./vault-1 -exec touch -t 200412231300 {} \;
@@ -58,79 +82,87 @@ find ./vault-3 -exec touch -t 200502241300 {} \;
 find ./vault-4 -exec touch -t 200503151300 {} \;
 
 # -----------------
-# iteration 1
+echo "Iteration 1..."
 # -----------------
 
-# copy Parts&Vendors(TM) database after iteration
-cp /home/dale/pv-1.mdb /usr/home/samba/maestro/pv.mdb
-./export_current_to_csv.sh
-#tar -zcvf /home/dale/pv-1.tar.gz /home/samba/maestro/csv/
+# copy Parts&Vendors(TM) database
+# archive mode preserves file times
+cp -a ./pv-1.mdb /home/samba/maestro/pv.mdb
+# copy csv
+cp -a ./csv-1/* /home/samba/maestro/csv
+# re-generate pv csv from mdb
+#./export_current_to_csv.sh
 
 # copy iteration files on top of remotefs
-# preserve file times -p
-cp -r -p ./vault-1/* /usr/home/samba/maestro/remotefs
+cp -a ./vault-1/* /home/samba/maestro/remotefs
 
 # rsync remote fs to vault
-echo "rsync - iteration 1"
-cp -a ./vault-scc-1/* /usr/home/samba/maestro/remotefs/
-rsync -a --itemize-changes --backup --backup-dir="./backup" --suffix=-`date +%FT%T` --log-file="./sync-1.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault > sync.out
+#rsync -a --itemize-changes --backup --backup-dir="./backup" --suffix=-`date +%FT%T` --log-file="./sync-1.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault > sync.out
+echo ""; echo "Iteration 1" > run_iterations_rsync.log
+rsync -a --itemize-changes --backup --suffix=-`date +%FT%T` --log-file="./run_iterations_rsync-1.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault >> ./run_iterations_rsync.log
 echo ""
  
 # -----------------
-# iteration 2
+echo "Iteration 2..."
 # -----------------
 
-cp /home/dale/pv-2.mdb /home/samba/maestro/pv.mdb
-./export_current_to_csv.sh
-#tar -zcvf /home/dale/pv-2.tar.gz /home/samba/maestro/csv/
+# copy Parts&Vendors(TM) database
+# archive mode preserves file times
+cp -a ./pv-2.mdb /home/samba/maestro/pv.mdb
+# copy csv
+cp -a ./csv-2/* /home/samba/maestro/csv
+# re-generate pv csv from mdb
+#./export_current_to_csv.sh
 
 # copy iteration files on top of remotefs
-# preserve file times -p
-cp -r -p ./vault-2/* /usr/home/samba/maestro/remotefs
+cp -a ./vault-2/* /home/samba/maestro/remotefs
 
 # rsync remote fs to vault
-echo "rsync - iteration 2"
-rm -r ./remotefs/*
-cp -a ./vault-scc-2/* ./remotefs/
-rsync -a --itemize-changes --backup --backup-dir="./backup" --suffix=-`date +%FT%T` --log-file="./sync-2.log" remotefs/ vault >> sync.out
+#rsync -a --itemize-changes --backup --backup-dir="./backup" --suffix=-`date +%FT%T` --log-file="./sync-1.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault > sync.out
+echo ""; echo "Iteration 2" >> run_iterations_rsync.log
+rsync -a --itemize-changes --backup --suffix=-`date +%FT%T` --log-file="./run_iterations_rsync-2.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault >> ./run_iterations_rsync.log
 echo ""
 
 # -----------------
-# iteration 3
+echo "Iteration 3..."
 # -----------------
 
-cp /home/dale/pv-3.mdb /home/samba/maestro/pv.mdb
-./export_current_to_csv.sh
-#tar -zcvf /home/dale/pv-3.tar.gz /home/samba/maestro/csv/
+# copy Parts&Vendors(TM) database
+# archive mode preserves file times
+cp -a ./pv-3.mdb /home/samba/maestro/pv.mdb
+# copy csv
+cp -a ./csv-3/* /home/samba/maestro/csv
+# re-generate pv csv from mdb
+#./export_current_to_csv.sh
 
 # copy iteration files on top of remotefs
-# preserve file times -p
-cp -r -p ./vault-3/* /usr/home/samba/maestro/remotefs
+cp -a -p ./vault-3/* /home/samba/maestro/remotefs
 
 # rsync remote fs to vault
-echo "rsync - iteration 3"
-rm -r ./remotefs/*
-cp -a ./vault-scc-3/* ./remotefs/
-rsync -a --itemize-changes --backup --backup-dir="./backup" --suffix=-`date +%FT%T` --log-file="./sync-3.log" remotefs/ vault >> sync.out
+#rsync -a --itemize-changes --backup --backup-dir="./backup" --suffix=-`date +%FT%T` --log-file="./sync-1.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault > sync.out
+echo ""; echo "Iteration 3" >> run_iterations_rsync.log
+rsync -a --itemize-changes --backup --suffix=-`date +%FT%T` --log-file="./run_iterations_rsync-3.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault >> ./run_iterations_rsync.log
 echo ""
 
 # -----------------
-# iteration 4
+echo "Iteration 4..."
 # -----------------
 
-cp /home/dale/pv-4.mdb /home/samba/maestro/pv.mdb
-./export_current_to_csv.sh
-#tar -zcvf /home/dale/pv-4.tar.gz /home/samba/maestro/csv/
+# copy Parts&Vendors(TM) database
+# archive mode preserves file times
+cp -a ./pv-4.mdb /home/samba/maestro/pv.mdb
+# copy csv
+cp -a ./csv-4/* /home/samba/maestro/csv
+# re-generate pv csv from mdb
+#./export_current_to_csv.sh
 
 # copy iteration files on top of remotefs
-# preserve file times -p
-cp -r -p ./vault-4/* /usr/home/samba/maestro/remotefs
+cp -a -p ./vault-4/* /home/samba/maestro/remotefs
 
 # rsync remote fs to vault
-echo "rsync - iteration 4"
-rm -r ./remotefs/*
-cp -a ./vault-scc-4/* ./remotefs/
-rsync -a --itemize-changes --backup --backup-dir="./backup" --suffix=-`date +%FT%T` --log-file="./sync-4.log" remotefs/ vault >> sync.out
+#rsync -a --itemize-changes --backup --backup-dir="./backup" --suffix=-`date +%FT%T` --log-file="./sync-1.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault > sync.out
+echo ""; echo "Iteration 4" >> run_iterations_rsync.log
+rsync -a --itemize-changes --backup --suffix=-`date +%FT%T` --log-file="./run_iterations_rsync-4.log" /usr/home/samba/maestro/remotefs/ /usr/home/samba/maestro/vault >> ./run_iterations_rsync.log
 echo ""
 
 # -----------------
