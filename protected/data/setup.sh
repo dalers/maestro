a1224c236f4bf9d1bdb4b6ab329df377a64599e2
Dale Scott <dale@dalescott.net>
2013-12-30 11:59:04 -0700

    some cleanup of initial config procedure

--- protected/data/setup.sh
+++ protected/data/setup.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
 # Setup Maestro server
-# - run when initially setting up maestro (see update.sh
-#   for updating afterwards)
+# - sets up initial maestro environment (also see update.sh)
+# - MUST be executed from maestro/protected/data/
+# - ASSUMES /usr/home/samba/maestro/ exists and is empty!!
 # - file paths hard-coded for server
 #
 # History
@@ -11,63 +12,80 @@
 #
 
 echo
-echo "Set up maestro..."
-echo ""
+echo "Setting up new Maestro install..."
+echo
 
-echo "Set shell scripts executable..."
+echo "Set shell scripts executable"
 chmod u+x /usr/local/www/maestro/protected/data/*.sh
 chmod u+x /usr/local/www/maestro/protected/yiic
-echo ""
+echo
 
-echo "Set permissions on directories written to
+echo "Set permissions on Yii runtime directories"
 chmod a+rw /usr/local/www/maestro/assets/
-chmod a+rw /usr/local/www/maestro/runtime/
-echo ""
+chmod a+rw /usr/local/www/maestro/protected/runtime/
+echo
 
-#echo "Create log file for export_current and load_current shell scripts..."
+echo "Clean smb share"
+rm -r /usr/home/samba/maestro/*
+echo
+
 # LOGGING NOT IMPLEMENTED
-#touch /usr/home/samba/maestro/legacy-export-import.log
-# give system cron user write permission to log file, but not Windows clients accessing share
-#chown nobody:wheel /usr/home/samba/maestro/maestro-import-from-legacy.log
-#chmod a+rw /usr/home/samba/maestro/maestro-import-from-legacy.log
-#echo ""
+#echo "Create load_current log file"
+#touch /usr/home/samba/maestro/load_current.log
+# system cron user gets write permission, others read-only (e.g. Windows clients)
+#chown nobody:wheel /usr/home/samba/maestro/load_current.log
+#chmod a+rw /usr/home/samba/maestro/load_current.log
+#echo
 
-echo "Create csv dir in maestro file directory for export_current shell script"
+echo "Extract maestro-scc-files-xxx.tar.gz temporarily to local directory"
+# will create ./maestro-scc-files/*
+# all copying of extracted files must use -a option to preserve file timestamps
+tar -xzf ./maestro-scc-files-1.0.1.tar.gz 
+echo
+
+echo "Copy SCC Parts&Vendors(TM) database to smb share"
+cp -a ./maestro-scc-files/pv-4.mdb /usr/home/samba/maestro/pv.mdb
+chmod a+rw /usr/home/samba/maestro/*.mdb
+echo
+
+echo "Bootstrap load_current by copying 'current' SCC CSV files to smb share"
 mkdir /usr/home/samba/maestro/csv
 chmod a+xrw /usr/home/samba/maestro/csv
-echo ""
-
-echo "Copy demo Parts&Vendors(TM) database to Windows share..."
-cp /usr/local/www/maestro/protected/data/pv/pv.mdb /usr/home/samba/maestro/
-chmod a+rw /usr/home/samba/maestro/*.mdb
-echo ""
-
-echo "Bootstrap export_current process with initial csv files..."
-cp /usr/local/www/maestro/protected/data/csv/*.csv /usr/home/samba/maestro/csv/
+cp -a ./maestro-scc-files/csv-4/*.csv /usr/home/samba/maestro/csv/
 chmod a+rw /usr/home/samba/maestro/csv/*.csv
-echo ""
+echo
 
-echo "Copy demo spreadsheet files to Windows share..."
-cp /usr/local/www/maestro/protected/data/excel/*.xlsx /usr/home/samba/maestro/
-# make read/write for Windows clients to update
+echo "Copy SCC spreadsheet files to smb share"
+cp -a ./maestro-scc-files/excel/*.xlsx /usr/home/samba/maestro/
+# all Windows clients get read write permission for updating
 chown nobody:wheel /usr/home/samba/maestro/*.xlsx
 chmod a+rw /usr/home/samba/maestro/*.xlsx
-echo ""
+echo
 
-echo "Copy README to Windows share..."
+echo "Copy README file to smb share"
 cp /usr/local/www/maestro/protected/data/README-winshare.txt /usr/home/samba/maestro/README.txt
-# ensure file has Windows EOL, but do not give write permission to Windows clients
+# ensure file has Windows EOL
 flip -u /usr/home/samba/maestro/README.txt
+# do not give write permission to Windows clients (as-is, without using chown/chmod to grant permission)
 #chown nobody:wheel /usr/home/samba/maestro/README.txt
 #chmod a+rw /usr/home/samba/maestro/README.txt
-echo ""
+echo
 
+# TODO CREATE load_current_unix.sh
+# not required when using bootstrap CSV and export_current_to_csv.sh is NOT used (i.e. no cron job)
 #echo "Load current data from CSV files..."
 #/usr/local/www/maestro/protected/data/load_current_unix.sh
-#echo ""
+#echo
 
 # cleanup
 # -------
+echo "Cleaning up..."
+echo
+
+echo "Delete local temp maestro-scc-files/ directory"
+rm -r ./maestro-scc-files/
+echo
+
 echo "Done!"
-echo ""
+echo
 exit 0
