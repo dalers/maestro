c346fe3bd8e88c5a6ced6f97f42aa327d0717c95
Mark Gajudo <mgajudo@gmail.com>
2014-01-01 18:15:22 -0700

    Fixes to OM db load script

--- protected/data/load_demo_om.sql
+++ protected/data/load_demo_om.sql
@@ -11,16 +11,16 @@ SET time_zone = "+00:00";
 -- Add user defined fields
 
 ALTER TABLE `tbl_project` 
-ADD COLUMN `user1` VARCHAR(255) NULL DEFAULT NULL AFTER `milestone_date`,
-ADD COLUMN `user2` VARCHAR(255) NULL DEFAULT NULL AFTER `user1`,
-ADD COLUMN `user3` VARCHAR(255) NULL DEFAULT NULL AFTER `user2`,
-ADD COLUMN `user4` VARCHAR(255) NULL DEFAULT NULL AFTER `user3`,
-ADD COLUMN `user5` VARCHAR(255) NULL DEFAULT NULL AFTER `user4`,
-ADD COLUMN `user6` VARCHAR(255) NULL DEFAULT NULL AFTER `user5`,
-ADD COLUMN `user7` VARCHAR(255) NULL DEFAULT NULL AFTER `user6`,
-ADD COLUMN `user8` VARCHAR(255) NULL DEFAULT NULL AFTER `user7`,
-ADD COLUMN `user9` VARCHAR(255) NULL DEFAULT NULL AFTER `user8`,
-ADD COLUMN `user10` VARCHAR(255) NULL DEFAULT NULL AFTER `user9`;
+ADD COLUMN `user1` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user2` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user3` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user4` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user5` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user6` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user7` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user8` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user9` VARCHAR(255) NULL DEFAULT NULL,
+ADD COLUMN `user10` VARCHAR(255) NULL DEFAULT NULL;
 
 -- --------------------------------------------------------
 
@@ -38,7 +38,7 @@ CREATE TABLE `tbl_om_order` (
   `create_user_id` int(11) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `update_user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`,`iteration`),
+  PRIMARY KEY (`id`),
   KEY `fk_order_to_create_user` (`create_user_id`),
   KEY `fk_order_to_update_user` (`update_user_id`),
   KEY `fk_order_to_project_idx` (`project_id`),
