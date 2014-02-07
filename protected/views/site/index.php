47bb9b4e394dcb41a1fda5e7ee4c9390f8c358b0
Dale Scott <dale@dalescott.net>
2014-02-06 19:39:39 -0700

    standardized wording (README.md, wiki, app)

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -6,6 +6,6 @@ $this->pageTitle=Yii::app()->name;
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-<p>Maestro(TM) supports design and manufacturing teams with product data management, including internal and external part numbers, drawings and other files, and supplier (vendor) information. Maestro is licensed according to the business friendly BSD 2-Clause License and is copyright by its authors.</p>
+<p>Maestro(TM) is a web-based application for design, manufacturing and sales teams to manage and share information related to parts, projects, material (simple, serialized, or lot controlled), issues and people. Maestro consolidates "current" information from spreadsheets, shared directories and single-purpose applications in an environment of *Transparency with Trust*. A complete data set for a well-defined going concern, the fictional Swift Construction Company (SCC), is included with Maestro. Maestro is an open source project, using the permissive business-friendly (<a href:"http://opensource.org/licenses/BSD-2-Clause">BSD 2-Clause License</a>.</p>
 
 <p>For more information on Maestro, refer to <a href="http://www.github.com/maestrobpm/maestro">Maestro project repository</a> and <a href="http://www.github.com/maestrobpm/maestro/wiki">Maestro project wiki</a>.</p>
