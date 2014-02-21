294448ebbfa984551f77c80e793560b6bbfd93c9
Dale Scott <dale@dalescott.net>
2014-02-20 21:36:55 -0700

    clean up, removed dos2unix utility

--- protected/data/load_current_from_csv.sh
+++ protected/data/load_current_from_csv.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 #
-# Load current data into Maestro from CSV
+# Load current data from CSV
 # - DATABASE ROOT PASSWORD IN PLAIN TEXT (LOAD DATA INFILE requires root user)
-# - HARDCODED file paths (assumes Maestro reference server)
+# - HARDCODED file paths
 #
 
-echo "load_current_from_csv: deleting data in maestro db"
-# current data will replace old data
-#/usr/local/bin/mysql -uroot -pappleton --local-infile=1 --show-warnings --verbose < /usr/local/www/maestro/protected/data/clear_tables.sql
-/usr/local/bin/mysql -uroot -pappleton --local-infile=1 < /usr/local/www/maestro/protected/data/clear_tables.sql
+echo "load_current_from_csv: deleting data in db"
+# current data overwrites old data
+#/usr/local/bin/mysql -uroot -pappleton --local-infile=1 --show-warnings --verbose maestro < /usr/local/www/maestro/protected/data/clear_tables.sql
+/usr/local/bin/mysql -uroot -pappleton --local-infile=1 maestro < /usr/local/www/maestro/protected/data/clear_tables.sql
 echo
 
 echo "load_current_from_csv: loading csv"
-#/usr/local/bin/mysql -uroot -pappleton --local-infile=1 --show-warnings --verbose < /usr/local/www/maestro/protected/data/load_current_from_csv.sql
-/usr/local/bin/mysql -uroot -pappleton --local-infile=1 < /usr/local/www/maestro/protected/data/load_current_from_csv.sql
+#/usr/local/bin/mysql -uroot -pappleton --local-infile=1 --show-warnings --verbose maestro < /usr/local/www/maestro/protected/data/load_current_from_csv.sql
+/usr/local/bin/mysql -uroot -pappleton --local-infile=1 maestro < /usr/local/www/maestro/protected/data/load_current_from_csv.sql
 echo
 
 exit 0
