ec4f09253a352383e1e5f3164658563b3c36428f
Dale Scott <dale@dalescott.net>
2014-10-01 20:21:56 -0600

    revised ldap structure

--- scc/bin/setup_ldap.sh
+++ scc/bin/setup_ldap.sh
@@ -11,18 +11,21 @@
 # -f    read entry modification from file
 #
 
-# create LDAP domain, manager and organizational units
-ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/domainmgr_scc_People.ldif
+# create LDAP domain and manager
+ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/domainmgr.ldif
+
+# create LDAP organizational unit
+ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/people.ldif
 
 # convert csv export from people master spreadsheet to ldif format
-csv2ldif2.pl -b 'ou=People,ou=scc,dc=root,dc=org' < /usr/local/maestro/scc/ods/person-ldap.csv > /usr/local/maestro/scc/ldif/person-ldap.csv.ldif
+csv2ldif2.pl -b 'ou=People,dc=root,dc=org' < /usr/local/maestro/scc/ods/person-ldap.csv > /usr/local/maestro/scc/ldif/person-ldap.csv.ldif
 
-# MANUALLY add uid=auth attribute to dn=Administrator User
-# TODO programatically add uid=auth attribute to dn=Administrator User
+# MANUALLY add attribute "uid: admin" to "dn: cn=Administrator User,ou=People,dc=root,dc=org" entry
+# SAVE-AS person-ldap.csv.edit1-admin.ldif
 
-# create LDAP users
-#ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/person-ldap.csv.corrected-admin.ldif
+# add scc users
+#ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/person-ldap.csv.ldif
 # use edited file
-ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/person-ldap.csv.corrected-admin.ldif
+ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/person-ldap.csv.edit1-admin.ldif
 
 exit 0
