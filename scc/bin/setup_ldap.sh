13ba45bac9551ce073a1ba7e71b36ce45339a7fa
Dale Scott <dale@dalescott.net>
2014-10-06 20:01:59 -0600

    fixups

--- scc/bin/setup_ldap.sh
+++ scc/bin/setup_ldap.sh
@@ -11,11 +11,11 @@
 # -f    read entry modification from file
 #
 
-# create LDAP domain and manager
-ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/domainmgr.ldif
+# create domain and manager
+ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldap/domainmgr.ldif
 
-# create LDAP organizational unit
-ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/people.ldif
+# create organizational unit
+ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldap/people.ldif
 
 # convert csv export from people master spreadsheet to ldif format
 #
@@ -23,15 +23,15 @@ ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestr
 #  source:  /home/samba/scc/csv/person-ldap.csv
 #  target:  /home/samba/scc/csv/person-ldap.csv
 #
-csv2ldif2.pl -b 'ou=People,dc=root,dc=org' < /usr/local/maestro/scc/ods/person-ldap.csv > /usr/local/maestro/scc/ldif/person-ldap.csv.ldif
+csv2ldif2.pl -b 'ou=People,dc=root,dc=org' < /usr/local/maestro/scc/ods/person-ldap.csv > /usr/local/maestro/scc/ldap/person-ldap.csv.ldif
 
 # MANUALLY add attribute "uid: admin" to "dn: cn=Administrator User,ou=People,dc=root,dc=org" entry
 # SAVE-AS person-ldap.csv.edit1-admin.ldif
 # TODO fix this
 
 # add scc users
-#ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/person-ldap.csv.ldif
+#ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldap/person-ldap.csv.ldif
 # use edited file
-ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldif/person-ldap.csv.edit1-admin.ldif
+ldapadd -v -x -D "cn=Manager,dc=root,dc=org" -w appleton -c -f /usr/local/maestro/scc/ldap/person-ldap.csv.edit1-admin.ldif
 
 exit 0
