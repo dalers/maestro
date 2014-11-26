9d57bd53c9419f14601d7974c0dec448941f93bb
Dale Scott <dale@dalescott.net>
2014-11-26 00:04:27 -0700

    standardized passwords

--- scc/bin/setup_adduser_centos.sh
+++ scc/bin/setup_adduser_centos.sh
@@ -7,27 +7,27 @@
 #   r = remove home dir
 #
 
-adduser bjenks -c "Barcoe Jenks" ; echo bjenks:sTr@temeyer | chpasswd
-adduser bswift -c "Barton Swift" ; echo bswift:sTr@temeyer | chpasswd
-adduser barmstrong -c "Bub Armstrong" ; echo barmstrong:sTr@temeyer | chpasswd
-adduser fmason -c "Frank Mason" ; echo fmason:sTr@temeyer | chpasswd
-adduser gjackson -c "Garret Jackson" ; echo gjackson:sTr@temeyer | chpasswd
-adduser hbaldwin -c "Hank Baldwin" ; echo hbaldwin:sTr@temeyer | chpasswd
-adduser hrandall -c "Helen Randall" ; echo hrandall:sTr@temeyer | chpasswd
-adduser jwood -c "Jacab Wood" ; echo jwood:sTr@temeyer | chpasswd
-adduser jperiod -c "James Period" ; echo jperiod:sTr@temeyer | chpasswd
-adduser jhaddon -c "Jennie Haddon" ; echo jhaddon:sTr@temeyer | chpasswd
-adduser jmorse -c "Jennie Morse" ; echo jmorse:sTr@temeyer | chpasswd
-adduser jsharp -c "John Sharp" ; echo jsharp:sTr@temeyer | chpasswd
-adduser mbaggert -c "Martha Baggert" ; echo mbaggert:sTr@temeyer | chpasswd
-adduser mnestor -c "Mary Nestor" ; echo mnestor:sTr@temeyer | chpasswd
-adduser mblair -c "Minnie Blair" ; echo mblair:sTr@temeyer | chpasswd
-adduser mdelazes -c "Miquel DeLazes" ; echo mdelazes:sTr@temeyer | chpasswd
-adduser nnewton -c "Ned Newton" ; echo nnewton:sTr@temeyer | chpasswd
-adduser rsampson -c "Rad Sampson" ; echo rsampson:sTr@temeyer | chpasswd
-adduser smalloy -c "Sarah Malloy" ; echo smalloy:sTr@temeyer | chpasswd
-adduser tswift -c "Tom Swift" ; echo tswift:sTr@temeyer | chpasswd
-adduser wdamon -c "Wakefield Damon" ; echo wdamon:sTr@temeyer | chpasswd
-adduser wcrawford -c "William Crawford" ; echo wcrawford:sTr@temeyer | chpasswd
+adduser bjenks -c "Barcoe Jenks" ; echo bjenks:appleton | chpasswd
+adduser bswift -c "Barton Swift" ; echo bswift:appleton | chpasswd
+adduser barmstrong -c "Bub Armstrong" ; echo barmstrong:appleton | chpasswd
+adduser fmason -c "Frank Mason" ; echo fmason:appleton | chpasswd
+adduser gjackson -c "Garret Jackson" ; echo gjackson:appleton | chpasswd
+adduser hbaldwin -c "Hank Baldwin" ; echo hbaldwin:appleton | chpasswd
+adduser hrandall -c "Helen Randall" ; echo hrandall:appleton | chpasswd
+adduser jwood -c "Jacab Wood" ; echo jwood:appleton | chpasswd
+adduser jperiod -c "James Period" ; echo jperiod:appleton | chpasswd
+adduser jhaddon -c "Jennie Haddon" ; echo jhaddon:appleton | chpasswd
+adduser jmorse -c "Jennie Morse" ; echo jmorse:appleton | chpasswd
+adduser jsharp -c "John Sharp" ; echo jsharp:appleton | chpasswd
+adduser mbaggert -c "Martha Baggert" ; echo mbaggert:appleton | chpasswd
+adduser mnestor -c "Mary Nestor" ; echo mnestor:appleton | chpasswd
+adduser mblair -c "Minnie Blair" ; echo mblair:appleton | chpasswd
+adduser mdelazes -c "Miquel DeLazes" ; echo mdelazes:appleton | chpasswd
+adduser nnewton -c "Ned Newton" ; echo nnewton:appleton | chpasswd
+adduser rsampson -c "Rad Sampson" ; echo rsampson:appleton | chpasswd
+adduser smalloy -c "Sarah Malloy" ; echo smalloy:appleton | chpasswd
+adduser tswift -c "Tom Swift" ; echo tswift:appleton | chpasswd
+adduser wdamon -c "Wakefield Damon" ; echo wdamon:appleton | chpasswd
+adduser wcrawford -c "William Crawford" ; echo wcrawford:appleton | chpasswd
 
 exit 0
