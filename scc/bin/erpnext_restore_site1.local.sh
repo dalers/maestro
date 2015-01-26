a1e9554d599f0a534e68c3b1de956292aad5c20d
Dale Scott <dale@dalescott.net>
2015-01-26 12:41:04 -0700

    Update erpnext_restore_site1.local.sh

--- scc/bin/erpnext_restore_site1.local.sh
+++ scc/bin/erpnext_restore_site1.local.sh
@@ -6,12 +6,16 @@
 #
 
 # restore database
-# create backup and drop database first
-gunzip < site1.local.sql.gza | mysql -u root -pbVUJVRiRBjNAc2mL
+# recommend backup first!
+# drop and create empty site1.local database
+mysql -u root -pbVUJVRiRBjNAc2mL -e 'DROP DATABASE `site1.local`'
+mysql -u root -pbVUJVRiRBjNAc2mL -e 'CREATE DATABASE `site1.local`'
+# load database
+gunzip < database.sql.gza | mysql -u root -pbVUJVRiRBjNAc2mL site1.local
 
 # restore files
 cd /home/frappe/frappe-bench/sites/site1.local/public
 mv files files.bak
 mkdir files
-tar -xzf /root/site1.local.files.tar.gz
+tar -xzf /root/files.tar.gz
 chown -R frappe:frappe files
