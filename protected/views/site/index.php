4aafe21d007596bd3acd9bb20fd094d85c2e510f
Dale Scott <dale@dalescott.net>
2015-03-11 02:24:35 +0000

    cleanup

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,6 +4,11 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1><?php echo CHtml::encode(Yii::app()->name); ?> v0.0.1</h1>
+<p>Welcome to Maestro, the Swift Construction Company's ERP system.
 
-<p>Maestro<sup>TM</sup> is a web-based application for design, manufacturing and sales teams to manage and share information related to parts, projects, material, issues and people. Maestro places "current" information under control in an environment of *Transparency with Trust*.</p>
+<p><b><em>Maestro</em></b><sup>TM</sup> is an <b><em>ERP for Engineers</em></b>. Maestro supports design, manufacturing and sales teams by managing information in an environment of <b><em>Transparency with Trust</em></b>.</p>
+
+<p>Maestro controls information related to parts, projects, stock, issues and people. Taking inspiration from the popular <em>Parts&Vendors<sup>TM</sup></em>, Maestro adds additional enterprise features and true multi-user operation.</p>
+
+<p>For more information, see the <a href="http://www.github.com/dalers/maestro/">Maestro development project on GitHub</a>.<p>
+
