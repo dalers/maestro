615438ccc37d1d298741deda4316812e2cb6ae7f
Dale Scott <dale@dalescott.net>
2015-03-15 22:06:02 -0600

    clarification

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,11 +4,12 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<p>Welcome to Maestro, the Swift Construction Company's ERP system.
+<p>Welcome to <b><em>Maestro</em></b><sup><em>TM</em></sup>, the Swift Construction Company's engineering ERP system.</p>
 
-<p><b><em>Maestro</em></b><sup>TM</sup> is an <b><em>ERP for Engineers</em></b>. Maestro supports design, manufacturing and sales teams by managing information in an environment of <b><em>Transparency with Trust</em></b>.</p>
+<p>Maestro is the <b><em>ERP for Engineers</em></b><sup><em>TM</em></sup>, managing information related to projects, parts, vendors and clients, stock (material), and issues.</p>
 
-<p>Maestro controls information related to parts, projects, stock, issues and people. Taking inspiration from the popular <em>Parts&Vendors<sup>TM</sup></em>, Maestro adds additional enterprise features and true multi-user operation.</p>
+<p>Design, manufacturing and support teams can be confident their data has <b><em>Transparency with Trust</em></b><sup><em>TM</em></sup>.</p>
+
+<p>Maestro takes inspiration from <a href="http://www.trilogydesign.com"><em>Parts&Vendors</em></a><sup><em>TM</em></sup>, adding additional enterprise features and true multi-user operation. Maestro also integrates with <a href="http://www.ganttproject.biz/"><em>GanttProject</em></a>, for users desiring a Gantt-based project management experience. Maestro is open source, licensed according to the permissive business-friendly BSD license.</p>
 
 <p>For more information, see the <a href="http://www.github.com/dalers/maestro/">Maestro development project on GitHub</a>.<p>
-
