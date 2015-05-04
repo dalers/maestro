f6e95e69bd5da0850c992f224bc39d625ff91fb7
Dale Scott <dale@dalescott.net>
2015-05-04 15:21:12 -0600

    update as per model

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -164,7 +164,7 @@ class UserController extends Controller
 	 */
 	protected function performAjaxValidation($model)
 	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='User-form')
+		if(isset($_POST['ajax']) && $_POST['ajax']==='user-form')
 		{
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
--- protected/models/Issue.php
+++ protected/models/Issue.php
@@ -231,8 +231,8 @@ class Issue extends CActiveRecord
 	 */ 
 	public function getStatusText()
 	{
-		//$statusOptions=$this->statusOptions;
-		$statusOptions=$this->getStatusOptions();
+		// following is equivalent to $statusOptions=$this->getStatusOptions();
+		$statusOptions=$this->statusOptions;
 		return isset($statusOptions[$this->status_id]) ? $statusOptions[$this->status_id] : "unknown status ({$this->status_id})";
 	}
 
