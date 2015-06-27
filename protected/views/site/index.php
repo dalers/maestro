b9b4a1cee446afb91ad3118fa2e12c14eebd93c4
Dale Scott <dale@dalescott.net>
2015-06-27 15:51:21 -0600

    edit for clarity

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -12,20 +12,14 @@ $this->pageTitle=Yii::app()->name;
 </p>
 <?php endif;?>
 
-<p><b><em>Maestro</em></b><sup>TM</sup> is <b><em>The ERP for 
-Engineers</em></b><sup>TM</sup>, an open-source ERP system
-providing <b><em>Transparency with Trust</em></b><sup>TM</sup> to
-design, manufacturing and support teams.</p> 
-
-<p>Maestro manages information relating to projects, parts, stock, 
-issues and people. Drawing inspiration from <a 
-href="http://www.trilogydesign.com/"><em>Parts&Vendors</em></a><sup><em> 
-TM</em></sup>, Maestro includes enterprise features, single-entry 
-accounting, and true multi-user operation.</p> 
-
 <p>This is the demo for the <a 
-href="http://www.github.com/dalers/maestro/">Maestro ERP project</a>. Maestro
-includes complete data for the (fictional) 
-<b><em>Swift Construction Company</em></b> (the <b><em>SCC</em></b>),
-including projects, parts, documents, stock (un-serialized, serialized
-and lot controlled), issues and people.</p> 
+		href="http://www.github.com/dalers/maestro/">Maestro ERP project</a>.</p>
+
+<p><b><em>Maestro</em></b><sup>TM</sup> is the <b><em>ERP for Engineers</em></b><sup>TM</sup>, an open-source
+enterprise resource planning system that provides <b><em>Transparency with Trust</em></b><sup>TM</sup>
+to design, manufacturing and support teams. Maestro manages information relating to projects, parts,
+stock, issues and people.</p>
+
+<p>Maestro includes complete data for the (fictional) <b><em>Swift Construction
+Company</em></b> (the <b><em>SCC</em></b>), including projects, parts, documents,
+stock (un-serialized, serialized and lot controlled), issues and people.</p> 
