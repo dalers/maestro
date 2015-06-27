ea54c6450b98a49beca7b804fa04f213605d7332
Dale Scott <dale@dalescott.net>
2015-06-27 00:41:01 -0600

    consistent admin views

--- protected/views/customer/index.php
+++ protected/views/customer/index.php
@@ -8,8 +8,21 @@ $this->breadcrumbs=array(
 
 $this->menu=array(
 	array('label'=>'Create Customer', 'url'=>array('create')),
-	array('label'=>'Manage Customer', 'url'=>array('admin')),
+	//array('label'=>'Manage Customer', 'url'=>array('admin')),
 );
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$('#customer-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
 ?>
 
 <h1>Customers</h1>
@@ -18,6 +31,13 @@ $this->menu=array(
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
 or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
 </p>
+
+<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
+<div class="search-form" style="display:none">
+<?php $this->renderPartial('_search',array(
+	'model'=>$model,
+)); ?>
+</div><!-- search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'customer-grid',
@@ -51,8 +71,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		'CUStateTaxID',
 		'CUNoPhonePrefix',
 		*/
-		/*
 		array('class'=>'CButtonColumn',
-		), */
+		),
 	),	
 )); ?>
