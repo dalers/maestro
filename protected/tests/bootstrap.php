d343ee3846f5747104948e8984663843a92943fa
Dale Scott <dale@dalescott.net>
2015-08-20 15:28:16 -0600

    use Composer to install PHPUnit

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -5,6 +5,7 @@ $yiit=dirname(__FILE__).'/../../../yii/framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
 require_once($yiit);
+require_once('../vendor/autoload.php');
 require_once(dirname(__FILE__).'/WebTestCase.php');
 
 Yii::createWebApplication($config);
--- protected/tests/unit/DbTest.php
+++ protected/tests/unit/DbTest.php
@@ -3,6 +3,7 @@ class DbTest extends CTestCase
 {
     public function testConnection()
     {
+        //$this->assertTrue(true);
         $this->assertNotNull(Yii::app()->db->connectionString);
     }
 }
